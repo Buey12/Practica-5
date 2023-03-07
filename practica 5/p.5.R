7d72b65ef091a77c14736c5f95e3ca1a5b0784c5
Buey12 <127197583+Buey12@users.noreply.github.com>
2023-03-07 12:20:12 +0100

    Update p.5.R

--- practica 5/p.5.R
+++ practica 5/p.5.R
@@ -4,4 +4,12 @@ numArtefactos <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 
 mean(numArtefactos) # 45.684210526316
 ej1 <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
 mean(ej1) 
-
+mode(numArtefactos) # 10
+length(ej1)
+sort(ej1)[56]
+sort(ej1)[78]
+median(ej1)
+numArtefactos <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
+tail(sort(table(numArtefactos)))
+barplot(sort(table(numArtefactos)),las = 2, col = "red"
+          grid()
