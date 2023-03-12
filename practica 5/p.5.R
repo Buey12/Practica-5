b75012fb42c194c64a7c700f4957630185a3ce2b
Buey12 <127197583+Buey12@users.noreply.github.com>
2023-03-12 13:35:38 +0100

    Update p.5.R

--- practica 5/p.5.R
+++ practica 5/p.5.R
@@ -1,24 +1,3 @@
-vector c(1,2,3,4,5)
-
-numArtefactos <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
-mean(numArtefactos) # 45.684210526316
-ej1 <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
-mean(ej1) 
-mode(numArtefactos) # 10
-length(ej1)
-sort(ej1)[56]
-sort(ej1)[78]
-median(ej1)
-
-numArtefactos <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
-tail(sort(table(numArtefactos)))
-
-table(numArtefactos)
-
-quantile(numArtefactos) # 0% 25% 50% 75% 100% 
-
-IQR(numArtefactos) # 44
-
 #Práctica 3 - Github + ejercicios
 
 #Ejercicio 1 
